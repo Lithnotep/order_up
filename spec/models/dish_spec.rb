cfec84d844d023ce9f93bccd92991fb4b79f2eaf
Max Mitrani <mjmitrani168@gmail.com>
2020-05-29 09:57:01 -0600

    add dish method testing

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many :ingredients}
   end
+  describe Dish do
+    describe "total_calories method" do
+      it "can get total_calories" do
+        chef_1 = Chef.create(name: "John")
+        dish_1 = Dish.create(name: "Meatballs", description: "good food", chef_id: chef_1.id)
+        ingredient_1 = Ingredient.create(name: "Meat", calories: "100")
+        ingredient_2 = Ingredient.create(name: "Sauce", calories: "50")
+        DishIngredient.create!(dish: dish_1, ingredient: ingredient_1)
+        DishIngredient.create!(dish: dish_1, ingredient: ingredient_2)
+
+        expect(dish_1.total_calories).to eq(150)
+      end
+    end
+  end
 end
